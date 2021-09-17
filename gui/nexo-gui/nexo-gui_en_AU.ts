dcb998d145cc4c2ef8527d4fcb2bc4eb68de81f7
Brad Neimann <brad.neimann@hotmail.com>
2021-09-18 00:41:35 +1000

    Remember to add missing file from rename

---
+++ gui/nexo-gui/nexo-gui_en_AU.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_AU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="21"/>
+        <source>&amp;Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>&amp;Input List</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
