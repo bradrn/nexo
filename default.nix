03247ba681e6400c957032ef55837e54d1022a3e
Brad Neimann <brad.neimann@hotmail.com>
2021-10-11 17:00:56 +1100

    Get HLS working with Nix

--- default.nix
+++ default.nix
@@ -10,6 +10,10 @@
     ghcjs = ["nexo" "web"];
   };
 
+  shellToolOverrides = self: super: {
+    inherit (pkgs.haskell.packages.ghc865) haskell-language-server;
+  };
+
   # overrides = self: super: {
   #   megaparsec = self.callHackage "megaparsec" "9.0.1" {};
   #   recursion-schemes = self.callHackage "recursion-schemes" "5.2.2.1" {};
