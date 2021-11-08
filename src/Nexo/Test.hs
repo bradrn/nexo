813b1ef045278a20917ff48b50623550b5f3a929
Brad Neimann <brad.neimann@hotmail.com>
2021-11-08 18:50:14 +1100

    Move single-expr evaluation functions to main package

---
+++ src/Nexo/Test.hs
@@ -0,0 +1,34 @@
+module Nexo.Test where
+
+import qualified Data.Map.Strict as Map
+import Data.Traversable (for)
+
+import Nexo.Expr.Parse
+import Nexo.Expr.Type
+import Nexo.Interpret
+import Nexo.Sheet
+
+testEvalExpr :: String -> Maybe (PType, Value (ValueEnv Eval))
+testEvalExpr xstr = do
+    x <- parseMaybe pExpr xstr
+    let c = Cell "test" Nothing x Invalidated
+        s = Sheet $ Map.singleton 0 c
+        Sheet s' = evalSheet s
+    val <- cellValue <$> Map.lookup 0 s'
+    case val of
+        ValuePresent t v -> Just (t, v)
+        _ -> Nothing
+
+testEvalExprs :: [(String, String)] -> Maybe (PType, Value (ValueEnv Eval))
+testEvalExprs xstrs = do
+    cs <- for xstrs $ \(n, xstr) -> do
+        x <- parseMaybe pExpr xstr
+        pure $ Cell n Nothing x Invalidated
+    let s = Sheet $ Map.fromList $ zip [0..] cs
+        Sheet s' = evalSheet s
+    val <- cellValue <$> Map.lookup 0 s'
+    case val of
+        ValuePresent t v -> Just (t, v)
+        _ -> Nothing
+
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,40 +1,14 @@
 module Main where
 
-import Data.Traversable (for)
-
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import qualified Data.Map.Strict as Map
 
-import Nexo.Expr.Parse
 import Nexo.Expr.Type
 import Nexo.Expr.Unit
 import Nexo.Interpret
-import Nexo.Sheet
-
-testEvalExpr :: String -> Maybe (PType, Value (ValueEnv Eval))
-testEvalExpr xstr = do
-    x <- parseMaybe pExpr xstr
-    let c = Cell "test" Nothing x Invalidated
-        s = Sheet $ Map.singleton 0 c
-        Sheet s' = evalSheet s
-    val <- cellValue <$> Map.lookup 0 s'
-    case val of
-        ValuePresent t v -> Just (t, v)
-        _ -> Nothing
-
-testEvalExprs :: [(String, String)] -> Maybe (PType, Value (ValueEnv Eval))
-testEvalExprs xstrs = do
-    cs <- for xstrs $ \(n, xstr) -> do
-        x <- parseMaybe pExpr xstr
-        pure $ Cell n Nothing x Invalidated
-    let s = Sheet $ Map.fromList $ zip [0..] cs
-        Sheet s' = evalSheet s
-    val <- cellValue <$> Map.lookup 0 s'
-    case val of
-        ValuePresent t v -> Just (t, v)
-        _ -> Nothing
+import Nexo.Test
 
 main :: IO ()
 main = defaultMain tests
